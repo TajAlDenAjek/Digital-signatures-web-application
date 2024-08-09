35a92585338808ad1e1b0b5c49a727bccf75cfa2
dark <dark@dark>
2024-08-09 09:40:33 +0300

    set auth in cookies

--- src/features/auth/authSlice.tsx
+++ src/features/auth/authSlice.tsx
@@ -1,44 +1,59 @@
 import { RootState } from '../../app/store'
 import { createSlice } from '@reduxjs/toolkit'
-
+import Cookies from 'js-cookie';
 export type Permissions = "admin" | "governmentOfficer" |  "user"
 
 // type of user state information
 export type User = {
     id: number | null,
-    username: string | null,
+    firstName: string | null,
+    middleName:string|null,
+    lastName:string|null
     permission: Permissions | null,
     token: string | null | any
 }
 // intial State when the app starts
 let intiState: User = {
     id: null,
-    username: null,
+    firstName: null,
+    middleName:null,
+    lastName:null,
     permission: null,
     token:null
 }
+
 // get localstoragestate
-if (localStorage.getItem('auth') !== null) {
-    intiState = { ...JSON.parse(localStorage.getItem('auth') || '{}') as User }
-}
+intiState = { ...JSON.parse(Cookies.get('digital_signature_website_cookie') || '{}') as User }
+
 
 const authSlice = createSlice({
     name: 'auth',
     initialState: intiState,
     reducers: {
         setCredentials: (state, action) => {
-            const { id, username, token, permission } = action.payload
-            const user = { id, username, token, permission }
-            localStorage.setItem('auth', JSON.stringify(user))
-            state.id = id;
-            state.username = username;
-            state.permission = permission;
+            const { data,token } = action.payload
+            console.log(action.payload)
+            const user={
+                token,id:data?.id,
+                firstName:data?.firstName,
+                middleName:data?.middleName,
+                lastName:data?.lastName,
+                permission:data?.role
+            }
+            Cookies.set('digital_signature_website_cookie', JSON.stringify(user), {  secure: true });
+            state.id = user?.id;
+            state.firstName = data?.firstName;
+            state.middleName = data?.middleName;
+            state.lastName = data?.lastName;
+            state.permission = data?.permission;
             state.token = token
         },
         logOut: (state) => {
-            localStorage.removeItem('auth')
+            Cookies.remove('digital_signature_website_cookie')
             state.id = null
-            state.username = null
+            state.firstName = null
+            state.middleName = null
+            state.lastName = null
             state.permission = null
             state.token = null
         },
@@ -52,6 +67,6 @@ export default authSlice.reducer
 
 //getters
 export const selectCurrentId = (state: RootState) => state.auth.id
-export const selectCurrentUserName = (state: RootState) => state.auth.username
+export const selectCurrentFirstName = (state: RootState) => state.auth.firstName
 export const selectCurrentPermission = (state: RootState) => state.auth.permission
 export const selectCurrentToken = (state: RootState) => state.auth.token
