0ecc412e4ebbe2e7c8bd169d5bbfed75fa97aec8
dark <dark@dark>
2024-08-16 14:16:11 +0300

    crud table styles

--- src/components/CrudTable.tsx
+++ src/components/CrudTable.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { DeleteOutlined, EditOutlined, FolderViewOutlined, FundViewOutlined } from "@ant-design/icons";
-import { Popconfirm, Table } from "antd";
+import { DeleteOutlined, EditOutlined, FolderViewOutlined, FundViewOutlined, EyeOutlined } from "@ant-design/icons";
+import { Popconfirm, Table, Tooltip } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import './styles.scss'
 interface params {
@@ -10,58 +10,62 @@ interface params {
     endpoint: string
     route: string,
     actions?: any[]
-    defaultActions? : boolean 
+    defaultActions?: boolean
 }
 
 
-function CrudTable({ dataSource, columns, endpoint, route, actions , defaultActions  }: params) {
-    const navigate = useNavigate() ;
-    if(!actions || (actions && defaultActions) ){
-        if(!actions){
-            actions = [] ;
+function CrudTable({ dataSource, columns, endpoint, route, actions, defaultActions }: params) {
+    const navigate = useNavigate();
+    if (!actions || (actions && defaultActions)) {
+        if (!actions) {
+            actions = [];
         }
         actions = [
             ...actions,
             {
-                title: <FolderViewOutlined></FolderViewOutlined> ,
-                handler(record:any) {
+                title:
+                    <Tooltip title='view'>
+                        <EyeOutlined style={{ fontSize: '150%' }}></EyeOutlined>
+                    </Tooltip>
+                ,
+                handler(record: any) {
                     navigate(`${route}/${record.id}`)
                 },
             },
             {
-                title:  <DeleteOutlined></DeleteOutlined> ,
-                handler (record:any)  {        
-                    console.log(record) ;    
-                    const url = `${endpoint}/${record.id}` ;    
-                    
+                title: "Delete",
+                handler(record: any) {
+                    console.log(record);
+                    const url = `${endpoint}/${record.id}`;
+
 
                 },
-                render(record:any){
-                    
+                render(record: any) {
+
                     return <Popconfirm
-                            title="Delete"
-                            description="Are you sure to delete this task?"
-                            okText="Yes"
-                            cancelText="No"
-                            onConfirm={()=>this.handler(record)}
-                        >
-                            <a>
-                                <DeleteOutlined></DeleteOutlined>
-                            </a>
-                        </Popconfirm>
+                        title="Delete"
+                        description="Are you sure to delete this task?"
+                        okText="Yes"
+                        cancelText="No"
+                        onConfirm={() => this.handler(record)}
+                    >
+                        <a>
+                            <DeleteOutlined></DeleteOutlined>
+                        </a>
+                    </Popconfirm>
 
                 }
             },
             {
-                title: <EditOutlined></EditOutlined> ,
-                handler(record:any) {
+                title: <EditOutlined></EditOutlined>,
+                handler(record: any) {
                     navigate(`${route}/${record.id}/edit`)
                 },
             },
-        ]    
+        ]
 
     }
-        
+
 
     const actionsColumn: any[] = [
 
@@ -70,23 +74,23 @@ function CrudTable({ dataSource, columns, endpoint, route, actions , defaultActi
             fixed: 'right',
             width: 200,
             key: 'action',
-            render: ( _: any, record : any ) => {
+            render: (_: any, record: any) => {
 
-                return <>
+                return <div className="table-actions">
                     {
-                        actions?.map((action: any) => {                            
+                        actions?.map((action: any) => {
                             if (action?.render) {
                                 return action.render(record);
                             }
                             else {
-                                
+
                                 return <a onClick={() => action.handler(record)} > {action.title} </a>
                             }
                         })
                     }
 
 
-                </>
+                </div>
             }
         }
 
