45497bbcb18aaa835b5b19f8109c54342ceeec8b
dark <dark@dark>
2024-08-16 13:49:28 +0300

    sidebar styles

--- src/Layout/Sidebar/SideBarConstants.tsx
+++ src/Layout/Sidebar/SideBarConstants.tsx
@@ -1,4 +1,17 @@
-import { UserAddOutlined, FormOutlined, PicCenterOutlined, UserOutlined, TeamOutlined, DollarOutlined, FileTextOutlined } from '@ant-design/icons';
+import { 
+    UserAddOutlined, 
+    FormOutlined, 
+    PicCenterOutlined,
+    PaperClipOutlined,
+     UserOutlined, 
+     TeamOutlined, 
+     DollarOutlined, 
+     FileTextOutlined ,
+     SafetyCertificateFilled,
+     SignatureOutlined,
+     SecurityScanOutlined,
+     GroupOutlined
+} from '@ant-design/icons';
 import React, { Children } from 'react'
 
 interface SideBarPage {
@@ -11,19 +24,19 @@ interface SideBarPage {
 export const adminPages: SideBarPage[] = [
     {
         key: '1',
-        icon: React.createElement(UserOutlined),
+        icon: React.createElement(PaperClipOutlined),
         label: `Manage Documents`,
         url: '/admin/document'
     },
     {
         key: '2',
-        icon: React.createElement(UserOutlined),
+        icon: React.createElement(UserAddOutlined),
         label: `Manage Users`,
         url: '/admin/users'
     },
     {
         key: '3',
-        icon: React.createElement(UserOutlined),
+        icon: React.createElement(GroupOutlined),
         label: `Manage Goverment Officiers`,
         url: '/admin/goverment-officiers'
     }
@@ -32,13 +45,13 @@ export const adminPages: SideBarPage[] = [
 export const governmentOfficerPages: SideBarPage[] = [
     {
         key: '1',
-        icon: React.createElement(UserOutlined),
+        icon: React.createElement(PaperClipOutlined),
         label: `Manage Documents`,
         url: '/gov/document'
     },
     {
         key: '2',
-        icon: React.createElement(UserOutlined),
+        icon: React.createElement(UserAddOutlined),
         label: `Manage Users`,
         url: '/gov/users'
     }
@@ -46,40 +59,33 @@ export const governmentOfficerPages: SideBarPage[] = [
 
 export const userPages: SideBarPage[]|any = [
     {
-        key:'0',
-        icon:React.createElement(PicCenterOutlined),
-        label:'Settings',
-        children:[
-            {
-                key: '1',
-                icon: React.createElement(PicCenterOutlined),
-                label: `Profile`,
-                url: '/profile'
-            },
-        ]
+        key: '1',
+        icon: React.createElement(UserOutlined),
+        label: `Profile`,
+        url: '/profile'
     },
     
     {
         key: '2',
-        icon: React.createElement(PicCenterOutlined),
+        icon: React.createElement(PaperClipOutlined),
         label: `Manage Documents`,
         url: '/document'
     },
     {
         key: '3',
-        icon: React.createElement(PicCenterOutlined),
+        icon: React.createElement(SafetyCertificateFilled),
         label: `Digital identity`,
         url: '/digital-identity/generate'
     }, 
     {
         key: '4',
-        icon: React.createElement(PicCenterOutlined),
+        icon: React.createElement(SignatureOutlined),
         label: `Electonic Signature`,
         url: '/electronic-signature'
     },
     {
         key: '5',
-        icon: React.createElement(PicCenterOutlined),
+        icon: React.createElement(SecurityScanOutlined),
         label: `Fingerprint`,
         url: '/fingerprint'
     },  
