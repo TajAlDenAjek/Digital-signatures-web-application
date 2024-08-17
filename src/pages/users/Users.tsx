d5374f7442b10e9f867ede4c5021d471ca3da760
dark <dark@dark>
2024-08-17 22:04:24 +0300

    users table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import CrudTable from '../../components/CrudTable'
 import { usersColumn } from '../../constants/columns'
+import { useGetUsersQuery } from '../../features/users/usersApiSlice'
 import { fakeUsersDataSource } from '../../constants/fake'
 type params = {
   type: string ;
 }
 function Users({type}:params ) {
-  
+  const {data}=useGetUsersQuery({})
   return (
     <CrudTable
         columns={usersColumn}
-        dataSource={fakeUsersDataSource}
+        dataSource={data?.data}
         endpoint="/api/users"
         route="/users"
     />
